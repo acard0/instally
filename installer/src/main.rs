e0e9963b4b649544fee158ba17bb0883fc043ead
acard0 <doqukan-acr@hotmail.com>
2023-06-28 20:47:50 +0300

    refactor

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -16,7 +16,7 @@ use std::{sync::{Arc}};
 
 use eframe::{egui};
 use parking_lot::{Mutex};
-use workloads::{installer::*, abstraction::*, uninstaller::*, updater::{UpdaterWorkloadState, UpdaterAppWrapper}};
+use workloads::{installer::*, abstraction::*, uninstaller::*, updater::{UpdaterWorkloadState}};
 
 pub type InstallerContext = AppContext<InstallerWorkloadState>;
 pub type InstallerApp = InstallyApp<InstallerWorkloadState>;
@@ -35,16 +35,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::set_var("RUST_LOG", rust_log);  
     env_logger::init();
 
+    let payload_result = quick_xml::de::from_str(PAYLOAD.strip_prefix("###/PAYLOAD/###").unwrap());
+    let product = match payload_result {
+        Ok(r) => {
+            log::info!("Payload Product is valid. Using it.");
+            r
+        },
+        Err(_) => {
+            log::info!("Payload Product is not valid. Using dummy.");
+
+            //TODO: strip this from production
+            Product { // prototype 'Product' structure
+                name: "Tutucu".to_owned(),
+                publisher: "liteware".to_owned(),
+                product_url: "https://liteware.io".to_owned(),
+                control_script: "none".to_owned(),
+                target_directory: "C:\\Users\\doquk\\AppData\\Roaming\\liteware.io\\Tutucu".to_owned(),
+                repository: "https://cdn.liteware.xyz/instally/tutucu/release/".to_owned()
+            }
+        }
+    };
+
+    log::info!("Payload xml: {:?}", quick_xml::se::to_string(&product));
+
     // create app state holder that is thread-safe
     let app = Box::leak(Box::new(|| {
-        UpdaterApp::new(Product { // prototype 'Product' structure
-            name: "Tutucu".to_owned(),
-            publisher: "liteware".to_owned(),
-            product_url: "https://liteware.io".to_owned(),
-            control_script: "none".to_owned(),
-            target_directory: "C:\\Users\\doquk\\AppData\\Roaming\\liteware.io\\Tutucu".to_owned(),
-            repository: "https://cdn.liteware.xyz/instally/tutucu/release/".to_owned()
-        })
+        InstallerApp::new(product.clone())
     }))();
 
     // spawn worker thread
@@ -52,46 +68,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(async move {
 
 
+/*
+    let updater = UpdaterAppWrapper::new(clone_worker); 
+    let workload_result = updater.run().await;
 
-let updater = UpdaterAppWrapper::new(clone_worker); 
-let workload_result = updater.run().await;
-
-match workload_result {
-    Ok(()) => {
-        updater.set_result(WorkloadResult::Ok);
-        updater.set_workload_state(UpdaterWorkloadState::Done);
-        println!("Workload completed");
-    },
-    Err(err) => {
-        log::error!("\n{err:?}");
-        updater.set_result(WorkloadResult::Error(err.to_string()));
-        updater.set_workload_state(UpdaterWorkloadState::Interrupted(err.to_string()));
+    match workload_result {
+        Ok(()) => {
+            updater.set_result(WorkloadResult::Ok);
+            updater.set_workload_state(UpdaterWorkloadState::Done);
+            println!("Workload completed");
+        },
+        Err(err) => {
+            log::error!("\n{err:?}");
+            updater.set_result(WorkloadResult::Error(err.to_string()));
+            updater.set_workload_state(UpdaterWorkloadState::Interrupted(err.to_string()));
+        }
+        
     }
-}
+ */
 
  
 
-/*
+    /*
 
-let uninstaller = UninstallerWrapper::new(clone_worker); 
-let workload_result = uninstaller.run().await;
+        let uninstaller = UninstallerWrapper::new(clone_worker); 
+        let workload_result = uninstaller.run().await;
 
-match workload_result {
-    Ok(()) => {
-        uninstaller.set_result(WorkloadResult::Ok);
-        uninstaller.set_workload_state(UninstallerWorkloadState::Done);
-        println!("Workload completed");
-    },
-    Err(err) => {
-        log::error!("\n{err:?}");
-        uninstaller.set_result(WorkloadResult::Error(err.to_string()));
-        uninstaller.set_workload_state(UninstallerWorkloadState::Interrupted(err.to_string()));
-    }
-}
+        match workload_result {
+            Ok(()) => {
+                uninstaller.set_result(WorkloadResult::Ok);
+                uninstaller.set_workload_state(UninstallerWorkloadState::Done);
+                println!("Workload completed");
+            },
+            Err(err) => {
+                log::error!("\n{err:?}");
+                uninstaller.set_result(WorkloadResult::Error(err.to_string()));
+                uninstaller.set_workload_state(UninstallerWorkloadState::Interrupted(err.to_string()));
+            }
+        }
 
- */
+    */
         
-        /* 
+
         
         let installer = InstallerWrapper::new(clone_worker); 
         let workload_result = installer.run().await;
@@ -108,9 +126,6 @@ match workload_result {
                 installer.set_workload_state(InstallerWorkloadState::Interrupted(err.to_string()));
             }
         }
-        
-         */
-
     });
 
     // build native opts
@@ -137,3 +152,7 @@ match workload_result {
 
     Ok(())
 }
+
+
+// could not come with something more stupid
+const PAYLOAD: &str = "###/PAYLOAD/###<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?><Product><Name>Tutucu</Name><Publisher>liteware</Publisher><ProductUrl>https://liteware.io</ProductUrl><ControlScript>none</ControlScript><TargetDirectory>C:\\Users\\doquk\\AppData\\Roaming\\liteware.io\\Tutucu</TargetDirectory><Repository>https://cdn.liteware.xyz/instally/tutucu/release/</Repository></Product>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ";
