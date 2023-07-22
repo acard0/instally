d865ad271da718c7a2369aa0b253e4fed82edbd8
acard0 <doqukan-acr@hotmail.com>
2023-07-22 20:51:18 +0300

    embed required vcruntime libs on win

--- scaffold/build.rs
+++ scaffold/build.rs
@@ -1,8 +1,18 @@
 
 fn main() {
   if cfg!(target_os = "windows") {
+
+    // embed icon
     let mut res = winres::WindowsResource::new();
     res.set_icon("./icons/icons8-setup-64.ico");
     res.compile().unwrap();
+
+    /*
+      link vcruntime
+
+      could cross-compile instead of this but skia is used as software renderer 
+      by egui which is not cross-compiled
+     */
+    static_vcruntime::metabuild();
   }
 }
