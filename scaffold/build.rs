5a67bb02aacb6cc9a86360969c5ecd47221e4f13
acard0 <doqukan-acr@hotmail.com>
2025-06-16 12:53:34 +0300

    add manifest on 32bit windows

--- scaffold/build.rs
+++ scaffold/build.rs
@@ -1,21 +1,28 @@
 
-fn main() {
+fn main(){
     if cfg!(target_os = "windows") {
-      let mut res = winres::WindowsResource::new();
-      res.set_icon("./icons/icons8-setup-64.ico");
-      res.set_manifest(r#"
-      <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-        <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-          <security>
-            <requestedPrivileges>
-              <requestedExecutionLevel level="asInvoker" uiAccess="false"/>
-            </requestedPrivileges>
-          </security>
-        </trustInfo>
-      </assembly>
-    "#);
-    res.compile().unwrap();
+        let ptr_width = std::env::var("CARGO_CFG_TARGET_POINTER_WIDTH")
+            .expect("CARGO_CFG_TARGET_POINTER_WIDTH not set");
 
-    static_vcruntime::metabuild();
-  }
+        let mut res = winres::WindowsResource::new();
+        
+        if ptr_width == "32" {
+            res.set_manifest(r#"
+            <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+            <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+            <security>
+            <requestedPrivileges>
+            <requestedExecutionLevel level="asInvoker" uiAccess="false"/>
+            </requestedPrivileges>
+            </security>
+            </trustInfo>
+            </assembly>
+            "#);
+        }
+        
+        res.set_icon("./icons/icons8-setup-64.ico");
+        res.compile().unwrap(); 
+        
+        static_vcruntime::metabuild();
+    }
 }
